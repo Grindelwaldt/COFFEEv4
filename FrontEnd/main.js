1742a0d35da7fc781949ad0d44b6ba205757f530
Grindelwaldt <grunewalddavid12@gmail.com>
2025-08-22 15:23:43 +0200

    Fixed socket.io-client import issues

--- FrontEnd/main.js
+++ FrontEnd/main.js
@@ -1,6 +1,6 @@
-import io from "socket.io-client";
+// import { io } from "socket.io-client";
 
-socket = io("https://coffeev4.onrender.com");
+const socket = io("https://coffeev4.onrender.com");
 
 socket.on("connect", () => {
       console.log("Connected to server:", socket.id);
