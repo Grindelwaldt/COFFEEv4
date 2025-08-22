f96198aa97edea16ac0fe878866b494ec329435f
Grindelwaldt <grunewalddavid12@gmail.com>
2025-08-22 12:56:37 +0200

    Updated backend to use express platform

--- BackEnd/main.js
+++ BackEnd/main.js
@@ -1,25 +1,12 @@
-import http from "http";
-import fs from "fs";
-import path from "path";
-import { fileURLToPath } from "url";
+const express = require('express');
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const app = express();
+const PORT = 3000;
 
-const server = http.createServer((req, res) => {
-    const filePath = path.join(__dirname, "message.txt");
-
-    fs.readFile(filePath, "utf8", (err, data) => {
-        if (err) {
-            res.writeHead(500, { "Content-Type": "text/plain" });
-            res.end("Error reading file");
-        } else {
-            res.writeHead(200, { "Content-Type": "text/plain" });
-            res.end(data);
-        }
-    });
-});
-
-server.listen(3000, () => {
-    console.log("Server is running on port 3000");
-});
+app.listen(PORT, (error) =>{
+    if(!error)
+        console.log("Server is Successfully Running, and App is listening on port "+ PORT);
+    else 
+        console.log("Error occurred, server can't start", error);
+    }
+);
