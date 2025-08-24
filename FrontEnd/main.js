676379457474ff79d55cd1527c7889246695f5a7
Grindelwaldt <grunewalddavid12@gmail.com>
2025-08-24 10:37:36 +0200

    Added API calls to toggle coffee machine on and off

--- FrontEnd/main.js
+++ FrontEnd/main.js
@@ -1,7 +1,22 @@
 // import { io } from "socket.io-client";
 
 const socket = io("https://coffeev4.onrender.com");
+const coffee_toggle = document.getElementById("machine-toggle");
+let bOn = false;
+
+coffee_toggle.addEventListener("change", () => {
+    bOn = coffee_toggle.checked;
+    if (socket.connected) {
+        socket.emit("set-bon-status", bOn);
+    }
+});
 
 socket.on("connect", () => {
-      console.log("Connected to server:", socket.id);
+    console.log("Connected to server:", socket.id);
+
+    socket.on("bOn-status", (newbOn) => {
+        bOn = newbOn;
+        coffee_toggle.checked = bOn;
+
+    });  
 });
